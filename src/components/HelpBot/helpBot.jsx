534cb420e35d71cdf6fb90a1cb24f61004034ad8
PatrickMck34 <corkscrewhollow@gmail.com>
2024-03-12 21:41:50 -0400

    sunflower help page

--- src/components/HelpBot/helpBot.jsx
+++ src/components/HelpBot/helpBot.jsx
@@ -1,8 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import sunflower from '../../resources/sunflower.png';
 import './helpBot.css'
+import { Link } from 'react-router-dom';
 function HelpBot ({currentSection}) {
     const [showHelp, setShowHelp] = useState(true);
+    const [showMessage, setShowMessage] = useState(false);
+    const sunflowerRef = useRef(null);
+  
+    useEffect(() => {
+      const sunflowerElement = sunflowerRef.current;
+      const handleAnimationEnd = () => {
+        setShowMessage(true);
+      };
+  
+      sunflowerElement.addEventListener('animationend', handleAnimationEnd);
+      return () => {
+        sunflowerElement.removeEventListener('animationend', handleAnimationEnd);
+      };
+    }, []);
 
     const handleImageClick = () => {
       setShowHelp(prevShowHelp => !prevShowHelp);
@@ -32,21 +47,26 @@ function HelpBot ({currentSection}) {
             <h2 className="text-lg absolute items-center justify-center w-full flex font-semibold sunflower text-teal-600">Persephone</h2>
           </div>
         )}
-        <div className="justify-center items-center flex w-full mt-[10%]">
+        <div className="justify-center items-center flex w-full mt-[10%] ">
 
         <img 
           src={sunflower} 
           alt="sunflower" 
-          className=" h-36 flex mt-3 justify-center items-center animate-move duration-5000 ease-in-out sunflower "
+          ref={sunflowerRef}
+          className=" h-24 flex mt-3 justify-center items-center animate-move duration-5000 ease-in-out sunflower "
           onClick={handleImageClick}
           />
           </div>
-      {!showHelp && (
-          <div className=" flex flex-col w-48 text-sm font-semibold mt-4 ml-2 sunflower">
-            <p>{getHelpMessage()}</p>
-     
+          {showMessage && (
+        <div className="flex mr-3 flex-col absolute font-semibold w-fit mt-[10%] border-2 border-slate-900 bg-teal-700  text-pink-50 rounded-xl p-1 text-center">
+          <p>If you need help find the sunflower at the bottom of the sceen.</p>
+          <Link to={"/help2"}>
+            
+          <button  className="bg-teal-600 shadow-md border-2 border-teal-700 shadow-slate-400 mr-1 font-serif text-sm  mx-auto rounded-xl p-1  mt-5 mb-1 w-24">Got It!</button>
+          </Link>
         </div>
       )}
+     
       </div>
     
   
