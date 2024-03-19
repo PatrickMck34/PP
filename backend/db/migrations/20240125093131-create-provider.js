2c6b9d0d7d958f2105ec292c4d9720fe37713dee
PatrickMck34 <corkscrewhollow@gmail.com>
2024-03-19 14:17:51 -0400

    -s

--- backend/db/migrations/20240125093131-create-provider.js
+++ backend/db/migrations/20240125093131-create-provider.js
@@ -159,6 +159,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Providers');
+    await queryInterface.dropTable(options);
   }
 };
